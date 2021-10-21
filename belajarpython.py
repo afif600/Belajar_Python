30220028f2d27b4d889916270f5f4bd04883921a
Afif Firmansyah <afiffirmansyah600@gmail.com>
2021-10-21 10:34:23 +0700

    menambahkan file belajarpython.py

---
+++ belajarpython.py
@@ -0,0 +1,18 @@
+print ("Tugas Python")
+print ()
+nama = input("Masukan Nama Anda :")
+pgln = input("Masukan Panggilan :")
+nim = input ("Masukan NIM :")
+tglhr = input ("Masukan Tanggal Lahir :")
+tmlhr = input ("Masukan Tempat Lahir :")
+tlpn = input ("Masukan Telepon :")
+almt = input ("Masukan Alamat :")
+umur = input ("Masukan Umur Anda :")
+print ()
+print ("Assalamu'alaikum wr.wb")
+print ()
+print ("Izinkan saya memperkenalkan diri saya. nama saya",nama,"Tapi anda bisa memanggil saya",pgln,
+        "NIM saya",nim,"Saya lahir di",tmlhr,"Dan umur saya",umur,"tahun. Tempat tinggal saya di",almt,
+       ". Jadi, jika ada perlu silahkan hubungi saya dengan nomor",tlpn,)
+print ()
+print ("Wassalamu'alaikum wr.wb")
